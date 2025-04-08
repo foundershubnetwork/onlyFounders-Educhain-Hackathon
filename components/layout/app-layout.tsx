a65be7b2f18777233dce5e5971b88c8d7110b3df
vibekananda Champati <vibekanandac15@gmail.com>
2025-04-08 20:07:38 +0530

    added latest images and tools

--- components/layout/app-layout.tsx
+++ components/layout/app-layout.tsx
@@ -626,7 +626,7 @@ export function AppLayout({
                         
                       ) : (
                         <LoginButton />
-                      )}
+                )}
 
                 </NavigationMenuList>
               </NavigationMenu>

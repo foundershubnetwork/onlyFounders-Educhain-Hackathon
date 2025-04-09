9629fa0549cd155fd61efd6b35203a66b44877be
Aeshna0204 <97512618+Aeshna0204@users.noreply.github.com>
2025-04-09 10:55:28 +0530

    Update hardhat.config.js
    Signed-off-by: Aeshna0204 <97512618+Aeshna0204@users.noreply.github.com>

--- OnlyFoundersBackendCodeBase/src/nft/hardhat.config.js
+++ OnlyFoundersBackendCodeBase/src/nft/hardhat.config.js
@@ -8,10 +8,9 @@ module.exports = {
     edu: {
       url: "https://rpc.edu-chain.raas.gelato.cloud/fd947a6d7b1b4565b1bb1cab5e4048b5", // Replace with the RPC URL of the EDU blockchain
       // url:"https://rpc.open-campus-codex.gelato.digital",
-      accounts: [`0xd526f09224fc1ebb554f49812d0a0b86987fc3d7cba71ec4d358cc73bf4a5d06`], // Replace with your private key
+      accounts: ["0x" + process.env.PRIVATE_KEY], // Replace with your private key
     },
   },
   
 };
 
-// contract Address:  0xe5deE58A1BEF2E7CF8649dA944E128d00144E1Fc
